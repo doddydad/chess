28a7d3a746047cefe8b457d2ef2036a783570ccc
Andrew Rose <andrew.rosed@gmail.com>
2022-01-20 15:17:28 +0000

    upkeep

--- chess_engine.py
+++ chess_engine.py
@@ -1,9 +1,10 @@
-"""
+class Game_State():
+    """
 This class stores all information about the gamestate
 
 changes for later, change board storage to numpy array for speed
 """
-class Game_State():
+
     def __init__(self):
         # Board is 8x8 list, each element having 2 characters,[colour, piece]
         self.board = [
@@ -17,4 +18,3 @@ class Game_State():
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
         self.white_to_move = True
         self.move_log = []
-    
