e03f6feb7416c998a3e13c578a16c3e9928ab859
Andrew Rose <andrew.rosed@gmail.com>
2022-02-07 20:49:11 +0000

    knight and rook moves nwo working, pieces are an object, wanting to test how keyword break works cos currently my code is UGLY

--- classes.py
+++ classes.py
@@ -15,14 +15,14 @@ changes for later, change board storage to numpy array for speed
     def __init__(self):
         # Board is 8x8 list, each element having 2 characters,[colour, piece]
         self.board = [
-            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
-            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
-            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+            [Piece("bR"), Piece("bN"), Piece("bB"), Piece("bQ"), Piece("bK"), Piece("bB"), Piece("bN"), Piece("bR")],
+            [Piece("bP"), Piece("bP"), Piece("bP"), Piece("bP"), Piece("bP"), Piece("bP"), Piece("bP"), Piece("bP")],
+            [Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--")],
+            [Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--")],
+            [Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--")],
+            [Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--"), Piece("--")],
+            [Piece("wP"), Piece("wP"), Piece("wP"), Piece("wP"), Piece("wP"), Piece("wP"), Piece("wP"), Piece("wP")],
+            [Piece("wR"), Piece("wN"), Piece("wB"), Piece("wQ"), Piece("wK"), Piece("wB"), Piece("wN"), Piece("wR")]]
         self.white_to_move = True
 
         # Eventually should make this only one list and have them derive
@@ -35,7 +35,7 @@ changes for later, change board storage to numpy array for speed
         self.move_log.append(move)
         self.player_move_log.append(move.chess_notation())
         self.board[move.end_row][move.end_column] = move.start_piece
-        self.board[move.start_row][move.start_column] = "--"
+        self.board[move.start_row][move.start_column] = Piece("--")
         self.white_to_move = not self.white_to_move
 
     def undo_move(self):
@@ -54,26 +54,26 @@ changes for later, change board storage to numpy array for speed
         possible_moves = []
         for r in range(len(self.board)):
             for c in range(len(self.board[r])):
-                piece = self.board[r][c]
+                p = self.board[r][c]
 
-                if (piece[0] == "b" and self.white_to_move is False) or (piece[0] == "w" and self.white_to_move is True):
-                    if piece[1] == "P":
-                        for m in self.get_pawn_moves(r, c, piece):
+                if (p.colour == "b" and self.white_to_move is False) or (p.colour == "w" and self.white_to_move is True):
+                    if p.type == "P":
+                        for m in self.get_pawn_moves(r, c, p):
                             possible_moves.append(m)
-                    elif piece[1] == "R":
-                        for m in self.get_rook_moves(r, c, piece):
+                    elif p.type == "R":
+                        for m in self.get_rook_moves(r, c, p):
                             possible_moves.append(m)
-                    elif piece[1] == "N":
-                        for m in self.get_knight_moves(r, c, piece):
+                    elif p.type == "N":
+                        for m in self.get_knight_moves(r, c, p):
                             possible_moves.append(m)
-                    elif piece[1] == "B":
-                        for m in self.get_bishop_moves(r, c, piece):
+                    elif p.type == "B":
+                        for m in self.get_bishop_moves(r, c, p):
                             possible_moves.append(m)
-                    elif piece[1] == "Q":
-                        for m in self.get_queen_moves(r, c, piece):
+                    elif p.type == "Q":
+                        for m in self.get_queen_moves(r, c, p):
                             possible_moves.append(m)
-                    elif piece[1] == "K":
-                        for m in self.get_king_moves(r, c, piece):
+                    elif p.type == "K":
+                        for m in self.get_king_moves(r, c, p):
                             possible_moves.append(m)
 
         return possible_moves
@@ -84,56 +84,107 @@ changes for later, change board storage to numpy array for speed
     
     """ big set of moves here for piece specific logic"""
     def get_pawn_moves(self, r, c, piece):
+        """If the piece being tested is a pawn, this finds its moves"""
         pawn_moves = []
-        if piece[0] == "w":
+        if piece.colour == "w":
             # Moving forwards
-            if self.board[r-1][c] == "--":
+            if self.board[r-1][c].label == "--":
                 pawn_moves.append(Move([(r, c), (r-1, c)], self))
-                if self.board[r-2][c] == "--" and r == 6:
+                if self.board[r-2][c].label == "--" and r == 6:
                     pawn_moves.append(Move([(r, c), (r-2, c)], self))
 
             # Captures
             try:
-                if self.board[r-1][c-1][0] == "b":
+                if self.board[r-1][c-1].colour == "b":
                     pawn_moves.append(Move([(r, c), (r-1, c-1)], self))
-                if self.board[r-1][c+1][0] == "b":
+                if self.board[r-1][c+1].colour == "b":
                     pawn_moves.append(Move([(r, c), (r-1, c+1)], self))
+            
+            # For when it tests things outside the board
             except IndexError:
                 pass
-        if piece[0] == "b":
+
+
+        if piece.colour == "b":
             # Moving forwards
-            if self.board[r+1][c] == "--":
+            if self.board[r+1][c].label == "--":
                 pawn_moves.append(Move([(r, c), (r+1, c)], self))
-                if self.board[r+2][c] == "--" and r == 1:
+                if self.board[r+2][c].label == "--" and r == 1:
                     pawn_moves.append(Move([(r, c), (r+2, c)], self))
 
             # Captures
             try:
-                if self.board[r+1][c-1][0] == "w":
+                if self.board[r+1][c-1].colour == "w":
                     pawn_moves.append(Move([(r, c), (r+1, c-1)], self))
-                if self.board[r+1][c+1][0] == "w":
+                if self.board[r+1][c+1].colour == "w":
                     pawn_moves.append(Move([(r, c), (r+1, c+1)], self))
+            
+            # For when it tests things outside the board
             except IndexError:
                 pass
         
         
         return pawn_moves
-                
-
-    def get_rook_moves(self, r, c, piece):
-        return []
 
     def get_knight_moves(self, r, c, piece):
-        return []
+        """ If piece being tested is a knight, this returns its moves"""
+        # Should be complete
+        knight_moves = []
+
+        for x in [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2],
+                  [-1, 2], [-1, -2]]:
+            try:
+                if self.board[r+x[0]][c+x[1]].colour != piece.colour:
+                    knight_moves.append(Move([(r, c), (r+x[0], c+x[1])], self))
+            except IndexError:
+                pass
+
+        return knight_moves
 
     def get_bishop_moves(self, r, c, piece):
-        return []
+        """ If piece being tested is a bishop, this returns its moves"""
+        bishop_moves = []
+        for x in [[1, 1], [1, -1], [-1, -1], [-1, 1]]:
+            i = 1
+            while 0 <= r+(i*x[0]) < 8 and 0 <= c+(i*x[1]) < 8:
+                if self.board[r+(i*x[0])][c+(i*x[1])].colour != piece.colour:
+                    bishop_moves.append(Move([(r, c), (r+(i*x[0]), c+(i*x[1]))], self))
+                    if self.board[r+(i*x[0])][c+(i*x[1])].colour != "-":
+                        i += 100
+                    i += 1
+                else:
+                    i += 100    # Bad way to exit while loop
+
+        return bishop_moves
+
+    def get_rook_moves(self, r, c, piece):
+        """ If piece being tested is a rook, this returns its moves"""
+        rook_moves = []
+        return rook_moves
 
     def get_queen_moves(self, r, c, piece):
-        return []
+        """ If piece being tested is a queen, this returns its moves"""
+        queen_moves = []
+        for item in self.get_bishop_moves(r, c, piece):
+            queen_moves.append(item)
+        for item in self.get_rook_moves(r, c, piece):
+            queen_moves.append(item)
+
+        return queen_moves
 
     def get_king_moves(self, r, c, piece):
-        return []
+        """ If piece being tested is a king, this returns its moves"""
+        # Very incomplete, needs castling and not being allowed into check
+        king_moves = []
+
+        for x in [[1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0],
+                  [-1, 1], [0, 1]]:
+            try:
+                if self.board[r+x[0]][c+x[1]].colour != piece.colour:
+                    king_moves.append(Move([(r, c), (r+x[0], c+x[1])], self))
+            except IndexError:
+                pass
+        return king_moves
 
 
 class Move():
@@ -171,12 +222,12 @@ class Move():
         """converts the move as python sees it to standard chess notation for log
         can't do castling, promotion, or check atm"""
         notation = ""
-        if "P" not in self.start_piece:
-            notation += self.start_piece[1]
+        if "P" != self.start_piece.type:
+            notation += self.start_piece.type
         # Below line should only be used when there's ambiguity about what piece
         # could have moved. That's hard to detect atm
         # notation += self.get_rank_file(self.start_row, self.start_column)
-        if self.end_piece != "--":
+        if self.end_piece.label != "--":
             notation += "x"
         notation += self.get_rank_file(self.end_row, self.end_column)
         return notation
@@ -185,3 +236,45 @@ class Move():
         """Converts our row column system to how chess notation
         should actually look"""
         return self.cols_to_files[c] + self.rows_to_ranks[r]
+
+
+class Piece():
+    """ Contains the attributes of a piece, position though will be saved in
+    gamestate """
+
+    def __init__(self, label):
+        self.colour = label[0]
+        self.type = label[1]
+        self.picture = label
+        self.label = label
+
+        if self.type == "Q":
+            self.name = "Queen"
+            self.value = 9
+        elif self.type == "R":
+            self.name = "Rook"
+            self.value = 5
+        elif self.type == "B":
+            self.name = "Bishop"
+            self.value = 3
+        elif self.type == "N":
+            self.name = "Knight"
+            self.value = 3
+        elif self.type == "P":
+            self.name = "Pawn"
+            self.value = 1
+        # Not sure if I'll need to end up deleting this line
+        elif self.type == "K":
+            self.name = "King"
+            self.value = 99999
+
+    def __str__(self):
+        try:
+            return self.colour + " " + self.name.lower()
+        except AttributeError:
+            return "No piece"
+
+    def __eq__(self, other):
+        if isinstance(other, Piece):
+            return self.label == other.label
+        return False
